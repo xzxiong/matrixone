c7347198c60db34f9a281930911f85d180790df4
xzxiong <xzxiong@yeah.net>
2023-06-05 11:11:42 +0800

    Revert "fix test report_statement_test.go"
    This reverts commit bd657f75d21812ffd8cb5e35e135f5e1471ac623.

--- pkg/util/trace/impl/motrace/report_statement_test.go
+++ pkg/util/trace/impl/motrace/report_statement_test.go
@@ -18,13 +18,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"testing"
-	"time"
-
 	"github.com/google/uuid"
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
 )
 
 func TestStatementInfo_Report_EndStatement(t *testing.T) {
@@ -236,7 +235,7 @@ func TestStatementInfo_ExecPlan2Json(t *testing.T) {
 			}
 			s.SetSerializableExecPlan(p)
 			got, _ := s.ExecPlan2Json(ctx)
-			assert.Equalf(t, tt.want, string(got), "ExecPlan2Json()")
+			assert.Equalf(t, tt.want, got, "ExecPlan2Json()")
 
 			mapper := new(map[string]any)
 			err := json.Unmarshal([]byte(got), mapper)
